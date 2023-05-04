542e460ecc12bdb51564d1789626f7451a96cc7f
Md. Mahfujur Rahman <mahfujur15-2406@diu.edu.bd>
2023-05-04 07:15:52 +0600

    data loadded from server

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './Home.css'
 import { AuthContext } from '../../providers/AuthProviders';
 
 const Home = () => {
+    const [chefs,setChefs] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/chefs')
+        .then(res=>res.json())
+        .then(data => console.log(data))
+        .catch(error=>console.log(error))
+    },[])
     const {user} = useContext(AuthContext);
     return (
         <div className='container'>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -7,7 +7,6 @@ const Main = () => {
     return (
         <div>
             <Header></Header>
-            
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
